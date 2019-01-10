955cd8586be327c87ae468ad092b3023b73c4a3a
ellestar1 <epopiel@me.com>
2019-01-10 16:15:18 -0500

    2nd major Commit
    Image updates, carousel, other integral items.

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,35 @@
-$(document).ready(function () {
-
-  $('.first-button').on('click', function () {
-
-    $('.animated-icon1').toggleClass('open');
+(function() {
+ 
+  // store the slider in a local variable
+  var $window = $(window),
+      flexslider = { vars:{} };
+ 
+  // tiny helper function to add breakpoints
+  function getGridSize() {
+    return (window.innerWidth < 600) ? 2 :
+           (window.innerWidth < 900) ? 3 : 4;
+  }
+ 
+  $(function() {
+    SyntaxHighlighter.all();
   });
-  $('.second-button').on('click', function () {
-
-    $('.animated-icon3').toggleClass('open');
+ 
+  $window.on(function() {
+    $('.flexslider').flexslider({
+      animation: "slide",
+      animationLoop: false,
+      itemWidth: 210,
+      itemMargin: 5,
+      minItems: getGridSize(), // use function to pull in initial value
+      maxItems: getGridSize() // use function to pull in initial value
+    });
   });
-  $('.third-button').on('click', function () {
-
-    $('.animated-icon4').toggleClass('open');
+ 
+  // check grid size on resize event
+  $window.resize(function() {
+    var gridSize = getGridSize();
+ 
+    flexslider.vars.minItems = gridSize;
+    flexslider.vars.maxItems = gridSize;
   });
-});
+}());
