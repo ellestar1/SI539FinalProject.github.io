9ba2a7a347ed6eaf4e19e2db1a356c836380a778
ellestar1 <epopiel@me.com>
2019-01-26 20:29:26 -0500

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -25,7 +25,7 @@ $(function() {
             }
         }
         var y = o[n - 1];
-        a.css("background-color", y).find("h1").text(i[n - 1] + " " + t);
+        a.css("background-color", y).find("h6").text(i[n - 1] + " " + t);
         f.find("div").css("color", y);
         l.find(".today").css("background-color", y);
         d()
@@ -48,7 +48,7 @@ $(function() {
             f.append("<div>" + s[e].substring(0, 3) + "</div>")
         }
     }
-
+/* funct d draws date*/
     function d() {
         var t;
         var n = $("#calendar").css("width", e + "px");
@@ -59,7 +59,7 @@ $(function() {
         });
         n.find("#calendar_header").css({
             height: e * (1 / 7) + "px"
-        }).find('i[class^="icon-chevron"]').css("line-height", e * (1 / 7) + "px")
+        }).find('i[class*="fa-chevron-"]').css("line-height", e * (1 / 7) + "px")
     }
 /* GET ALL THE SYSTEM DATES!!!!!!*/
     function v(e, t) {
@@ -84,23 +84,23 @@ $(function() {
         n = e.getMonth() + 1
     }
     /* set partially dynamic date variables with colors, needed much changing to work with 4.5.1 accessibility*/
-    //renaming variables to somwthin
+    //renaming variables to somwthin//
     var e = 480;
     var t = 2013;
     var n = 9;
     var r = [];
     var i = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
     var s = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var o = ["#0074D9", "#fdb81e"]; 
-    // excess non-colorsafe colors do not use "#c0392b", "#27ae60", "#FF6860", "#f39c12", "#f1c40f", "#e67e22", "#2ecc71", "#e74c3c", "#d35400", "#2c3e50"];
+    var o = ["#0074D9"]; 
+    /* excess non-colorsafe colors do not use "#c0392b", "#27ae60", "#FF6860", "#f39c12", "#f1c40f", "#e67e22", "#2ecc71", "#e74c3c", "#d35400", "#2c3e50"];*/
     var u = $("#calendar");
     var a = u.find("#calendar_header");
     var f = u.find("#calendar_weekdays");
     var l = u.find("#calendar_content");
     b();
     c();
-    //call the correct chevron, could be done in jQuery, but good ole JS here is fine//
-    a.find('i[class^="fas fa-chevron-"]').on("click", function() {
+    /*call the correct chevron, could be done in jQuery, but good ole JS here is fine*/
+    a.find('i[class*="fa-chevron-"]').on("click", function() {
         var e = $(this);
         var r = function(e) {
             n = e == "next" ? n + 1 : n - 1;
@@ -120,3 +120,23 @@ $(function() {
         }
     })
 })
+
+function checkForm(){
+        var goodname = document.getElementById("fullname")
+        var goodemail = document.getElementById("streetaddr")
+        if (a.value.length == 0)
+            {
+                document.getElementById("nameerrormsg").style.display = "block"; a = 0;}
+           else {
+                document.getElementById("nameerrormsg").style.display = "none";}
+
+        if (b.value.length == 0)
+            {document.getElementById("addrerrormsg").style.display = "block"; b = 0}
+        else {document.getElementById("addrerrormsg").style.display = "none"; }
+
+        if (b == 0 || a == 0){
+            alert("NO!!")
+            return false;
+        }
+    }  
+
